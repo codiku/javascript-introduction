de69c9108794d68fbac436e1bdcc0624862a52c3
rlebhar-clone <160504776+rlebhar-clone@users.noreply.github.com>
2024-11-01 18:11:20 +0100

    variables

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-console.log("Hello world !");
+let firstName = "Robin";
+let lastName = "Hood";
+// let fullName = firstName + lastName;
+// let fullName = "Robin" + " " + "Hoody";
+let fullName = firstName + " " + lastName;
+let sentence = "Hello my name is " + fullName;
+let age = 32;
+sentence = sentence + " and I am " + age + " years old";
+console.log(sentence);
